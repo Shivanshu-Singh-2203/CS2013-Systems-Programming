bea4d3899c78b96200aaaa48c6bc406d365ac597
Shivanshu Singh <112401042@smail.iitpkd.ac.in>
2025-10-22 11:32:01 +0530

    Added rectangle struct

--- lab08/Assignments/src/4/4.c
+++ lab08/Assignments/src/4/4.c
@@ -1,51 +1,26 @@
 #include <stdio.h>
 
-// Define structures Rectangle and Point
-//TODO: Complete the code
-
-
-struct point {
-	float x ;
-	float y ;
+struct point{
+	float x;
+	float y;
 };
 
-typedef struct point Point;
-
+typedef struct point Point; 
 struct rectangle {
 	float height;
-	float length;
-	Point bottom;
+	float breadth;
+	Point leftcorner;
 };
 
 typedef struct rectangle Rectangle;
 
+int main(){
+	Rectangle r;
+	Point center;
+	scanf("%f %f %f %f", &r.leftcorner.x , &r.leftcorner.y, &r.height, &r.breadth);
+	center.x = r.leftcorner.x + r.breadth/2;
+	center.y = r.leftcorner.y + r.height/2;
+	printf("%.4f %.4f\n", center.x, center.y);
 
-Point FindCenter(const Rectangle r){
-	float y_displacement = r.height/2;
-	float x_displacement = r.length/2;
-	Point ce;
-	ce.x = r.bottom.x + x_displacement;
-	ce.y= r.bottom.y  + y_displacement;
-	return ce;
+	return 0;	
 }
-
-
-int main()
-{
-	Point corner_bl, center;
-	float height, width;
-
-	scanf("%f %f", &corner_bl.x, &corner_bl.y);
-	scanf("%f %f", &height, &width);
-	
-	Rectangle r = {height, width, corner_bl};
-	center = FindCenter(r);
-
-	printf("%0.4f %0.4f\n", center.x, center.y);
-
-
-	//TODO: Complete the code
-	//Do not add/modify anything below this line
-	return 0;
-}
-
